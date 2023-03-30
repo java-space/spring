05f85d10f9ac4d9ad68523d5bea2ec12ede088ca
mothusimolorane <molorane91@gmail.com>
2023-03-30 09:14:25 +0200

    first commit

---
+++ JpaLesson1/src/main/java/za/co/javaspace/JpaLesson1/JpaLesson1Application.java
@@ -0,0 +1,13 @@
+package za.co.javaspace.JpaLesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLesson1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLesson1Application.class, args);
+	}
+
+}
---
+++ JpaLesson1/src/test/java/za/co/javaspace/JpaLesson1/JpaLesson1ApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.javaspace.JpaLesson1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLesson1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
